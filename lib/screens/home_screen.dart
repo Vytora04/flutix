5e41a5f0f4c35f2d063aa02dff3bf9e04c642aac
Fahimsyach Lokanta <141541167+VytoraSNMz@users.noreply.github.com>
2025-05-29 11:40:42 +0700

    Initial commit for session6

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Screen')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Welcome to the Home Screen!'),
+            ElevatedButton(
+              onPressed: () {
+                // Add your logout logic here
+                Navigator.pop(context);
+              },
+              child: const Text('Logout'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
